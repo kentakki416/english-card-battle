78908c6bd06bb3fe04fed8372b7c9489e0634a10
Nagisa Kaworu <fujimori_kenta@cyberagent.co.jp>
2025-06-06 21:56:36 +0900

    feat: serverのCIを修正

--- server/jest.config.js
+++ server/jest.config.js
@@ -6,4 +6,22 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest',
   },
   testMatch: ['**/test/**/*.test.ts'], // テストファイルのパターンを指定
+  
+  // カバレッジ設定
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'text-summary', 'html'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,js}',
+    '!src/**/*.d.ts',
+    '!src/**/index.ts',  // エントリーポイントは除外
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 40,
+      functions: 40,
+      lines: 40,
+      statements: 40
+    }
+  }
 };
