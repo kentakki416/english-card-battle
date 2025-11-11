4089cdc42b1ce82488098451480f72b737906579
kentakki416 <fujimorikenta@icloud.com>
2025-11-11 10:12:23 +0900

    cdkを復活

---
+++ infrastructure/cdk/lib/constructs/api.ts
@@ -0,0 +1,48 @@
+// API Gateway+LambdaやLamdba FunctoinUrlなどの構成
+
+import * as cdk from 'aws-cdk-lib'
+import { CognitoUserPoolsAuthorizer, Cors, LambdaRestApi } from 'aws-cdk-lib/aws-apigateway'
+import { UserPool } from 'aws-cdk-lib/aws-cognito'
+import { TableV2 } from 'aws-cdk-lib/aws-dynamodb'
+import { Function, Runtime } from 'aws-cdk-lib/aws-lambda'
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
+import { Construct } from 'constructs'
+
+interface ApiProps extends cdk.StackProps {
+  userPool: UserPool
+  sampleTable: TableV2
+}
+
+export class Api extends Construct {
+  readonly testLambda: Function
+  constructor(scope: Construct, id: string, props: ApiProps) {
+    super(scope, id)
+
+    const lambdaFunction = new NodejsFunction(this, 'SampleLamdba', {
+      entry: 'src/index.ts',
+      handler: 'handler',
+      runtime: Runtime.NODEJS_LATEST,
+      environment: {
+        TABLE_NAME: props.sampleTable.tableName
+      }
+    })
+
+    props.sampleTable.grantReadWriteData(lambdaFunction)
+
+    const userPoolsAuthorizer = new CognitoUserPoolsAuthorizer(this, 'UserPoolsAuthroizer', {
+      cognitoUserPools: [props.userPool]
+    })
+
+    new LambdaRestApi(this, 'RestApi', {
+      handler: lambdaFunction,
+      defaultCorsPreflightOptions: {
+        allowOrigins: Cors.ALL_ORIGINS,
+        allowMethods: Cors.ALL_METHODS,
+        allowHeaders: Cors.DEFAULT_HEADERS,
+        maxAge: cdk.Duration.minutes(5)
+      },
+      deployOptions: { stageName: 'v1' },
+      defaultMethodOptions: { authorizer: userPoolsAuthorizer }
+    })
+  }
+}
---
+++ infrastructure/cdk/lib/constructs/ecr.ts
@@ -0,0 +1,34 @@
+import * as cdk from 'aws-cdk-lib'
+import { Repository, TagStatus } from 'aws-cdk-lib/aws-ecr'
+import { Construct } from 'constructs'
+
+interface EcrProps extends cdk.StackProps {
+  repositoryName: string
+}
+
+export class EcrConstruct extends Construct {
+  constructor(scope: Construct, id: string, props: EcrProps) {
+    super(scope, id)
+
+    const repository = new Repository(this, 'Repository', {
+      repositoryName: props.repositoryName,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      imageScanOnPush: true,
+      lifecycleRules: [
+        {
+          maxImageCount: 5,
+          tagStatus: TagStatus.ANY,
+        },
+        {
+          maxImageAge: cdk.Duration.days(1),
+          tagStatus: TagStatus.UNTAGGED,
+        }
+      ]
+    })
+
+    new cdk.CfnOutput(this, 'EcrRepositoryUri', {
+      value: repository.repositoryUri,
+      description: 'ECR Repository URI',
+    })
+  }
+}
